ac5f875adaccf0270d2a986fbcf3a01103ebc94c
iguanodon <greg.bunyea@gmail.com>
2017-10-18 18:49:33 -0400

    remove bad calls

--- test/helpers/input/variables.js
+++ test/helpers/input/variables.js
@@ -13,15 +13,3 @@ export const assign_b_to_a    = `var a = 4
                                  var b = 'hello'
                                  a = b` 
 
-// bad ones
-export const named_is_num     = `var 3`
-
-export const start_with_num   = `var 3hello`
-
-export const named_key_var    = `var var`
-
-export const named_key_if     = `var if`
-
-export const named_key_else   = `var else`
-
-export const named_key_for    = `var for`
